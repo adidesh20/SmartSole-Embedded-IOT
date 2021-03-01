4737eec3897c6b5e6d3d7d97559785f39d59d85e
Gautham <gr1518@ic.ac.uk>
2021-03-01 19:09:05 +0000

    pressure update

--- src/raspi/sensors/pressure.py
+++ src/raspi/sensors/pressure.py
@@ -2,14 +2,16 @@ from manometer import *
 bus = smbus2.SMBus(1)
 
 sensor = PressureSensor (bus)
-walk_threshold=16
-walk_count=0
+walk_threshold=15
+
 while True:
-    total=0
-    mode=-1
-    walk_total=0     
+
+    long_total=0     
     for y in range (100):
-                
+        total=0
+        walk_total=0
+        walk_count=0.001
+        mode=-1        
         for x in range(30):
             v=sensor.read()
             if v>mode:
@@ -19,10 +21,11 @@ while True:
                 walk_count+=1
 
             total+=v
+            long_total+=v
             time.sleep(0.1)
         avg= total/30
         walk_avg= walk_total/walk_count
         ## send mode,avg and walk_avg to server now
-    long_avg=total/3000
+    long_avg=long_total/3000
     ## send long_vg to server 
     
